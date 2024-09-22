57b3c5d125b1e12235ea018cc8eb53ba81a004cb
bhav380 <bhaweshrathour18@gmail.com>
2024-09-22 17:40:36 +0530

    feat: Display "Data not found" message when no data available
    Setted the default error parameter in the Error component to "Data not
    found" to ensure this message displays when data is unavailable.

--- web/src/components/Error.jsx
+++ web/src/components/Error.jsx
@@ -1,11 +1,11 @@
 import { Center, Heading } from '@chakra-ui/react';
 import useModeColors from '../hooks/useModeColors';
 
-export default function Error({ error = 'something went wrong' }) {
+export default function Error({ error = 'No Data found' }) {
   const { red } = useModeColors();
 
   return (
-    <Center>
+    <Center h="300px">
       <Heading size='lg' textTransform='capitalize' color={red}>
         {error}
       </Heading>
